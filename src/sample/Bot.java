be869452548f4cfbf010292203c77cb6f84944ec
ArtHell <temka19977@mail.ru>
2016-04-05 18:07:58 +0300

    lab 1,2 for review

--- src/sample/Bot.java
+++ src/sample/Bot.java
@@ -1,51 +1,51 @@
 package sample;
 
 public class Bot {
-	final int timeOfMoving = 2000;
-	Player player;
-	Ball ball;
-	int time;
-	boolean on;
+  final int timeOfMoving = 2000;
+  Player player;
+  Ball ball;
+  int time;
+  boolean on;
 
-	public Bot(Player player, Ball ball) {
-		this.player = player;
-		this.ball = ball;
-		time = 0;
-		on = false;
-	}
+  public Bot(Player player, Ball ball) {
+    this.player = player;
+    this.ball = ball;
+    time = 0;
+    on = false;
+  }
 
-	public void Execute() {
-		if (time < timeOfMoving) {
-			if (player.getTranslateX() + player.getWidth() / 2
-							< ball.getTranslateX() + ball.getWidth()) {
-				player.moveRight();
-			} else {
-				player.moveLeft();
-			}
-		}
-		if (time > timeOfMoving) {
-			if (player.getTranslateX() + player.getWidth() / 2
-							< ball.getTranslateX()) {
-				player.moveRight();
-			} else {
-				player.moveLeft();
-			}
-		}
-		if (time == timeOfMoving * 2) {
-			time = 0;
-		}
-		time++;
-	}
+  public void Execute() {
+    if (time < timeOfMoving) {
+      if (player.getTranslateX() + player.getWidth() / 2
+          < ball.getTranslateX() + ball.getWidth()) {
+        player.moveRight();
+      } else {
+        player.moveLeft();
+      }
+    }
+    if (time > timeOfMoving) {
+      if (player.getTranslateX() + player.getWidth() / 2
+          < ball.getTranslateX()) {
+        player.moveRight();
+      } else {
+        player.moveLeft();
+      }
+    }
+    if (time == timeOfMoving * 2) {
+      time = 0;
+    }
+    time++;
+  }
 
-	public boolean isOn() {
-		return on;
-	}
+  public boolean isOn() {
+    return on;
+  }
 
-	public void setOn() {
-		on = true;
-	}
+  public void setOn() {
+    on = true;
+  }
 
-	public void setOff() {
-		on = false;
-	}
+  public void setOff() {
+    on = false;
+  }
 }
--- src/sample/SimpleObject.java
+++ src/sample/SimpleObject.java
@@ -5,15 +5,15 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class SimpleObject extends Pane {
-	Rectangle rect;
+  Rectangle rect;
 
-	public SimpleObject(double x, double y, double w, double h, double sw,
-											Color fillColor, Color strokeColor) {
-		rect = new Rectangle(w, h, fillColor);
-		this.setTranslateX(x);
-		this.setTranslateY(y);
-		this.getChildren().add(rect);
-		rect.setStrokeWidth(sw);
-		rect.setStroke(strokeColor);
-	}
+  public SimpleObject(double x, double y, double w, double h, double sw,
+                      Color fillColor, Color strokeColor) {
+    rect = new Rectangle(w, h, fillColor);
+    this.setTranslateX(x);
+    this.setTranslateY(y);
+    this.getChildren().add(rect);
+    rect.setStrokeWidth(sw);
+    rect.setStroke(strokeColor);
+  }
 }
